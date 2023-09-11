7502c7326f33009827066de002f06070d3a19770
TAeE <98029204+Haemyon@users.noreply.github.com>
2023-09-12 02:41:33 +0900

    0912

---
+++ Source/Multiplay_Game/Multiplay_Game.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Multiplay_Game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Multiplay_Game, "Multiplay_Game" );
